0538973b47d973e149196d157ac63b46cbc5e41e
Patrick Walsh <pmwals09@gmail.com>
2024-02-08 15:54:21 -0500

    Beginning AST - 2.4

---
+++ parser/parser.go
@@ -0,0 +1,29 @@
+package parser
+
+import (
+	"github.com/pmwals09/monkey-interpreter/ast"
+	"github.com/pmwals09/monkey-interpreter/lexer"
+	"github.com/pmwals09/monkey-interpreter/token"
+)
+
+type Parser struct {
+	l         *lexer.Lexer
+	curToken token.Token
+	peekToken token.Token
+}
+
+func New(l *lexer.Lexer) *Parser {
+  p := &Parser{l: l}
+  p.nextToken()
+  p.nextToken()
+  return p
+}
+
+func (p *Parser) nextToken() {
+  p.curToken = p.peekToken
+  p.peekToken = p.l.NextToken()
+}
+
+func (p *Parser) ParseProgram() *ast.Program {
+  return nil
+}
